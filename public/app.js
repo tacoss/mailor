c197a53c39188cb32543b4058b66107b51c4212a
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-01-17 19:01:28 -0600

    Basic UI behavior; WIP

--- public/app.js
+++ public/app.js
@@ -1,8 +1,63 @@
+const {
+  bind, mount, render, listeners, attributes, classes,
+} = somedom;
+
+const $ = bind(render,
+  attributes({
+    class: classes,
+  }),
+  listeners());
+
+const mainEl = document.querySelector('#main');
+const toggleEl = document.querySelector('#toggle');
+
 async function main() {
   const req = await fetch('/templates.json');
   const data = await req.json();
 
-  console.log(data);
+  let lastLink;
+  let lastOption;
+
+  function untoggle(e, node) {
+    if (node) {
+      node.classList.remove('active');
+    }
+
+    e.target.classList.add('active');
+
+    return e.target;
+  }
+
+  function pickMe(node) {
+    lastOption = node;
+  }
+
+  function showMe(e) {
+    e.preventDefault();
+    lastLink = untoggle(e, lastLink);
+    mainEl.src = e.target.href;
+  }
+
+  function showData(e) {
+    e.preventDefault();
+    lastOption = untoggle(e, lastOption);
+    toggleEl.checked = true;
+  }
+
+  function showPreview(e) {
+    e.preventDefault();
+    lastOption = untoggle(e, lastOption);
+    toggleEl.checked = false;
+  }
+
+  mount('#list', ['ul', data.map(x => ['li', [
+    ['a', { href: `/${x}.html`, onclick: showMe }, x]
+  ]])], $);
+
+  mount('#opts', ['ul.flex', [
+    ['li', [['a.active', { href: '#', onclick: showPreview, oncreate: pickMe }, 'Preview']]],
+    ['li', [['a', { href: '#', onclick: showData }, 'Data']]],
+  ]], $);
 }
 
 main();
