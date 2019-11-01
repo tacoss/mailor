70fcf0d3515e172d60c53e090e7b4a5656292288
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-11-01 08:24:46 -0600

    Add more tests

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const fs = require('fs-extra');
 const path = require('path');
+const td = require('testdouble');
 const stdMocks = require('std-mocks');
 const stdinMock = require('mock-stdin');
 
@@ -9,11 +10,15 @@ const stdinMock = require('mock-stdin');
 describe('Mailor', () => {
   const Mailor = require('../lib/mailer');
 
+  function sendMail(opts, cb) {
+    return cb(null, opts);
+  }
+
   async function failure(tpl, ...args) {
     let e;
 
     try {
-      await tpl(...args);
+      e = await tpl(...args);
     } catch (_e) {
       e = _e;
     }
@@ -35,12 +40,84 @@ describe('Mailor', () => {
     expect(typeof Mailor.buildMailer(path.join(__dirname, 'fixtures'), { transport: true }).template).to.eql('function');
   });
 
-  // FIXME: unit testing for these methods?
-  // console.log(Mailor.shouldSkip);
-  // console.log(Mailor.render);
-  // console.log(mailer._sendMail);
-  // console.log(mailer.sendMail);
-  // console.log(mailer.close);
+  describe('_sendMail', () => {
+    it('should invoke any given transporter', async () => {
+      const mailer = new Mailor({ transport: { sendMail } });
+      const result = await mailer._sendMail({ test: true });
+
+      expect(result).to.eql({ test: true });
+    });
+  });
+
+  describe('sendMail', () => {
+    const template = path.join(__dirname, 'fixtures/template.html');
+    const data = { foo: 'bar' };
+    const email = 'admin@email.com';
+    const subject = 'Just testing out!';
+
+    let mailer;
+
+    beforeEach(() => {
+      mailer = new Mailor({
+        internalErrors: ['Example'],
+        transport: { sendMail },
+        maildev: true,
+      });
+    });
+
+    afterEach(() => {
+      td.reset();
+    });
+
+    it('should not send mails during testing', async () => {
+      td.replace(Mailor, 'shouldSkip', td.func('shouldSkip'));
+      td.when(Mailor.shouldSkip()).thenReturn(true);
+
+      const result = await mailer.sendMail({
+        template, data, email, subject,
+      });
+
+      expect(result).to.eql({ skip: true });
+    });
+
+    it('should render mustache from given body', async () => {
+      td.replace(Mailor, 'render', td.func('render'));
+      td.when(Mailor.render(template, td.matchers.isA(Object))).thenReturn('OK');
+
+      const result = await mailer.sendMail({
+        template, data, email, subject,
+      });
+
+      expect(result.html).to.eql('OK');
+    });
+
+    it('should catch all internalErrors on failure', async () => {
+      const e = new Error();
+
+      e.code = 'Example';
+
+      td.replace(mailer, '_sendMail', td.func('_sendMail'));
+      td.when(mailer._sendMail(td.matchers.isA(Object))).thenReject(e);
+
+      const result = await mailer.sendMail({
+        template, data, email, subject,
+      });
+
+      expect(result.originalError).to.eql(e);
+    });
+
+    it('should invoke transporter otherwise', async () => {
+      const result = await mailer.sendMail({
+        template, data, email, subject,
+      });
+
+      expect(result).to.eql({
+        to: 'admin@email.com',
+        html: 'It works! - Value: bar\n',
+        subject,
+      });
+    });
+  });
 
   it('should be able to invoke templates', async () => {
     stdMocks.use();
@@ -75,12 +152,11 @@ describe('Mailor', () => {
 
         expect(stdout[stdout.length - 1]).to.match(/---> MAIL: "template" N\/A -> user@email.com/);
         expect(emails.length).to.eql(1);
-        expect(emails[0].html).to.eql('It works!\n');
+        expect(emails[0].html).to.eql('It works! - Value: \n');
         expect(emails[0].subject).to.eql('template');
         expect(emails[0].to).to.eql([{ address: 'user@email.com', name: '' }]);
-        // FIXME: it should have return value?
-        console.log({ result });
-      }, 50);
+        expect(result.accepted).to.eql(['user@email.com']);
+      }, 500);
     });
   });
 });
@@ -128,7 +204,7 @@ describe('integration', () => {
           expect(stdout).to.eql([
             'MailDev SMTP Server running at 0.0.0.0:1025\n',
           ]);
-        }, 1000);
+        }, 500);
       });
     });
   });
